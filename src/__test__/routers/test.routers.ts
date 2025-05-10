54d3029138705ea23924ba807973d590bb6675dd
Aliaksei <talerqa@gmail.com>
2025-05-11 01:10:19 +0300

    add

--- src/__test__/routers/test.routers.ts
+++ src/__test__/routers/test.routers.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import {HttpStatus} from "../../core/types/httpCodes";
+import {db} from "../../core/db/db";
 
 export const testingRouter = Router({});
 
 testingRouter.delete('/all-data', (req, res) => {
-  res.status(HttpStatus.NoContent).send([]);
+
+  res.status(HttpStatus.NoContent).send(db.blogs = []);
 });
--- src/blogs/routers/blogs.routers.ts
+++ src/blogs/routers/blogs.routers.ts
@@ -14,6 +14,7 @@ import {
 
 export const blogsRouter = Router({});
 
+
 blogsRouter.get('', (req, res) => {
   const blogs = blogsRepository.findAllBlogs()
   res.status(HttpStatus.Ok).send(blogs);
