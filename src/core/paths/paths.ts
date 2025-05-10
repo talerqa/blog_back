c1c710af170030ea4bff83733149b36a5606849a
Aliaksei <talerqa@gmail.com>
2025-05-11 01:13:05 +0300

    add

--- src/core/paths/paths.ts
+++ src/core/paths/paths.ts
@@ -1,3 +1,3 @@
-export const BLOGS_PATH = '/api/blogs';
-export const POSTS_PATH = '/api/posts';
-export const TESTING_PATH = '/api/testing';
+export const BLOGS_PATH = '/blogs';
+export const POSTS_PATH = '/posts';
+export const TESTING_PATH = '/testing';
