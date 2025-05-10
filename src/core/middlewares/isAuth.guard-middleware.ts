b8a7a9785ba0ce99085cce742edab189d934f967
Aliaksei <talerqa@gmail.com>
2025-05-11 02:54:14 +0300

    add

--- src/core/middlewares/isAuth.guard-middleware.ts
+++ src/core/middlewares/isAuth.guard-middleware.ts
@@ -5,24 +5,29 @@ export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'qwerty';
 
 export const isAuthGuardMiddleware = (req, res, next) => {
   const auth = req.headers['authorization'] as string; // 'Basic xxxx'
-  if (!auth) {
-    res.sendStatus(HttpStatus.Unauthorized);
-    return;
+
+  if (auth !== 'Basic YWRtaW46cXdlcnR5') {
+    return res.sendStatus(HttpStatus.Unauthorized);
   }
 
-  const [authType, token] = auth.split(' '); //admin:qwerty
-  if (authType !== 'Basic') {
-    res.sendStatus(HttpStatus.Unauthorized);
-    return;
-  }
-
-  const credentials = Buffer.from(token, 'base64').toString('utf-8');
-  const [username, password] = credentials.split(':');
-
-  if (username !== ADMIN_USERNAME || password !== ADMIN_PASSWORD) {
-    res.sendStatus(HttpStatus.Unauthorized);
-    return;
-  }
+  // if (!auth) {
+  //   res.sendStatus(HttpStatus.Unauthorized);
+  //   return;
+  // }
+  //
+  // const [authType, token] = auth.split(' '); //admin:qwerty
+  // if (authType !== 'Basic') {
+  //   res.sendStatus(HttpStatus.Unauthorized);
+  //   return;
+  // }
+  //
+  // const credentials = Buffer.from(token, 'base64').toString('utf-8');
+  // const [username, password] = credentials.split(':');
+  //
+  // if (username !== ADMIN_USERNAME || password !== ADMIN_PASSWORD) {
+  //   res.sendStatus(HttpStatus.Unauthorized);
+  //   return;
+  // }
 
   next(); // Успешная авторизация, продолжаем
 };
