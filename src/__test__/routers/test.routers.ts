e327bc1be5161921c2308b6f40b4701834816e2a
Aliaksei <talerqa@gmail.com>
2025-05-11 03:05:07 +0300

    add

--- src/__test__/routers/test.routers.ts
+++ src/__test__/routers/test.routers.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import {HttpStatus} from "../../core/types/httpCodes";
+import {isAuthGuardMiddleware} from "../../core/middlewares/isAuth.guard-middleware";
 
 export const testingRouter = Router({});
 
-testingRouter.delete('/all-data', (req, res) => {
+
+testingRouter.delete('/all-data', isAuthGuardMiddleware, (req, res) => {
   res.status(HttpStatus.NoContent).send([]);
 });
--- src/setup-app.ts
+++ src/setup-app.ts
@@ -8,8 +8,8 @@ import {isAuthGuardMiddleware} from "./core/middlewares/isAuth.guard-middleware"
 export const setupApp = (app: Express) => {
   app.use(express.json());
 
-  app.use(BLOGS_PATH, blogsRouter)
-  app.use(TESTING_PATH, testingRouter);
+  app.use(BLOGS_PATH,  blogsRouter)
+  app.use(TESTING_PATH,  testingRouter);
   setupSwagger(app);
   return app;
 };
