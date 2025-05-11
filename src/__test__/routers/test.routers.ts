8245cf3aa77ab90e066e4d907389e610c0145ab9
Aliaksei <talerqa@gmail.com>
2025-05-11 03:07:38 +0300

    add

--- src/__test__/routers/test.routers.ts
+++ src/__test__/routers/test.routers.ts
@@ -1,10 +1,9 @@
 import {Router} from 'express';
 import {HttpStatus} from "../../core/types/httpCodes";
-import {isAuthGuardMiddleware} from "../../core/middlewares/isAuth.guard-middleware";
 
 export const testingRouter = Router({});
 
 
-testingRouter.delete('/all-data', isAuthGuardMiddleware, (req, res) => {
+testingRouter.delete('/all-data', (req, res) => {
   res.status(HttpStatus.NoContent).send([]);
 });
