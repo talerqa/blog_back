7ab67a4d6d1038135427dca029c53d199a182d4c
Aliaksei <talerqa@gmail.com>
2025-07-20 23:54:02 +0300

    fix comments

--- src/entity/auth/routers/handlers/authGuard.ts
+++ src/entity/auth/routers/handlers/authGuard.ts
@@ -4,43 +4,43 @@ import { findUserQueryRepo } from "../../../user/repositories/findUserQueryRepo"
 import jwt from "jsonwebtoken";
 
 export const authGuard = async (req: Request, res: Response, next) => {
-  // try {
-  const auth = req.headers["authorization"];
-  if (!auth) {
+  try {
+    const auth = req.headers["authorization"];
+    if (!auth) {
+      res.status(HttpStatus.Unauthorized).send();
+    }
+    const token = auth?.split(" ")[1];
+    const authType = auth?.split(" ")[0];
+
+    if (authType !== "Bearer" || !token) {
+      res.status(HttpStatus.Unauthorized).send();
+    }
+
+    const isVerify = jwt.verify(token, process.env.SECRET_KEY);
+    console.log(isVerify);
+    if (!isVerify) {
+      res.status(HttpStatus.Unauthorized).send();
+    }
+    const { userId } = isVerify;
+
+    const user = await findUserQueryRepo(userId);
+
+    if (!user) {
+      res.status(HttpStatus.Unauthorized).send();
+    }
+
+    // res.status(HttpStatus.Ok).send({
+    //   email: user?.email,
+    //   login: user?.login,
+    //   userId: userId
+    // });
+
+    req.headers = { userId };
+    next();
+    return;
+  } catch (e) {
+    console.log(e);
     res.status(HttpStatus.Unauthorized).send();
+    return;
   }
-  const token = auth?.split(" ")[1];
-  const authType = auth?.split(" ")[0];
-
-  if (authType !== "Bearer" || !token) {
-    res.status(HttpStatus.Unauthorized).send();
-  }
-
-  const isVerify = await jwt.verify(token, process.env.SECRET_KEY);
-  console.log(isVerify);
-  if (!isVerify) {
-    res.status(HttpStatus.Unauthorized).send();
-  }
-  const { userId } = isVerify;
-
-  const user = await findUserQueryRepo(userId);
-
-  if (!user) {
-    res.status(HttpStatus.Unauthorized).send();
-  }
-
-  // res.status(HttpStatus.Ok).send({
-  //   email: user?.email,
-  //   login: user?.login,
-  //   userId: userId
-  // });
-
-  req.headers = { userId };
-  next();
-  return;
-  // } catch (e) {
-  //   console.log(e);
-  //   res.status(HttpStatus.Unauthorized).send();
-  //   return;
-  // }
 };
